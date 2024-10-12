ec8c2e10ea2713f3f9aaf3066313b083a4241feb
Amit Hofree <amit.hofree@gmail.com>
2024-10-12 13:49:52 +0300

    Remove dependency

--- components/kelvinator/climate.py
+++ components/kelvinator/climate.py
@@ -4,22 +4,18 @@ from esphome.components import climate_ir
 from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
+CODEOWNERS = ["@amithofree"]
 
 kelvinator_ns = cg.esphome_ns.namespace("kelvinator")
 KelvinatorClimate = kelvinator_ns.class_("KelvinatorClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = cv.All(
-    climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(KelvinatorClimate),
-        }
-    ),
-    cv.only_with_arduino
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(KelvinatorClimate),
+    }
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
-    cg.add_library("crankyoldgit/IRremoteESP8266", "2.8.6")
-    
