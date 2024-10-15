13e82ed6c1cbad8eefceb1bcb8a42f2520f5f32a
Amit Hofree <amit.hofree@gmail.com>
2024-10-16 00:20:19 +0300

    More debugging

--- components/kelvinator/kelvinator_protocol.cpp
+++ components/kelvinator/kelvinator_protocol.cpp
@@ -52,7 +52,7 @@ void log_state_8(const uint8_t[] data, size_t length) {
 void KelvinatorProtocol::encode(RemoteTransmitData *dst, const KelvinatorData &data) {
   dst->set_carrier_frequency(38000);
   dst->reserve(2 * 2 * (6 + 8 * 8));  // Two messages, each message can carry 8 bytes, requires 6 extra bits, and each bit is one mark + space
-  log_state_8(data->data(), KELVINATOR_STATE_LENGTH);
+  log_state_8(data.data(), KELVINATOR_STATE_LENGTH);
 
   // There are two messages back-to-back in a full Kelvinator IR message
   // sequence.
@@ -76,7 +76,7 @@ void KelvinatorProtocol::encode(RemoteTransmitData *dst, const KelvinatorData &d
     dst->item(KELVINATOR_BIT_MARK, 2 * KELVINATOR_GAP_SPACE);
   }
 
-  log_state_32(*dst, dst->get_data().size());
+  log_state_32(dst->get_data(), dst->get_data().size());
 }
 
 void KelvinatorProtocol::encode_byte_(RemoteTransmitData *dst, uint8_t item) {
